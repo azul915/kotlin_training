3d04edb3a96f914f890b5e077170653a7968a363
azul915 <k.aoyagi940516@gmail.com>
2021-04-09 02:25:34 +0900

    update

--- src/kia/sec7/aggregator.kt
+++ src/kia/sec7/aggregator.kt
@@ -1,8 +1,27 @@
 package kia.sec7
+import java.math.BigDecimal
+import java.time.LocalDate
 
 fun sec7Output(): Any {
 //    return invokePlus()
-    return invokePointSamplePlus()
+//    return invokePointSamplePlus()
+//    return invokePointTimes()
+//    return invokeCharTimes()
+//    return invokeCompoundPlusOperator()
+//    return invokeFoo()
+//    return invokeBar()
+//    return invokeUnaryMinus()
+//    return invokeInc()
+//    return invokeBigDecimalInc()
+//    return invokeOverrideEqual()
+//    return invokeOverrideCompareTo()
+//    return invokeOverrideGetter()
+//    return invokeOverrideSetter()
+//    return invokeOverrideContains()
+//    return invokeOverrideRangeTo()
+//    return (0..10).forEach { println(it) }
+//    return invokeOverrideClosedRangeIterator()
+    return invokeOverrideNameComponents()
 }
 
 fun invokePlus() {
@@ -15,4 +34,104 @@ fun invokePointSamplePlus() {
     val p1 = PointSample(10, 20)
     val p2 = PointSample(30, 40)
     println(p1 + p2)
+}
+
+fun invokePointTimes() {
+    val p = Point(10, 20)
+    println(p * 1.5)
+}
+
+fun invokeCharTimes() {
+    println('a' * 3)
+}
+
+fun invokeCompoundPlusOperator() {
+    var point = Point(1, 2)
+    point += Point(3,4)
+    point.also { println(it) }
+}
+
+fun invokeFoo() {
+    val numbers = ArrayList<Int>()
+    numbers += 42
+    println(numbers[0])
+}
+
+fun invokeBar() {
+    val list = arrayListOf<Int>(1, 2)
+    list += 3
+    val newList = list + listOf<Int>(4, 5)
+    list.also { println(it) }
+    newList.also { println(it) }
+}
+
+fun invokeUnaryMinus() {
+    val p = Point(10, 20)
+    println(-p)
+}
+
+fun invokeInc() {
+    var p = Point(1, 1)
+    println(p++)
+    println(++p)
+}
+
+fun invokeBigDecimalInc() {
+    operator fun BigDecimal.inc() = this + BigDecimal.ONE
+
+    var bd = BigDecimal.ZERO
+
+    println(bd++)
+    println(++bd)
+}
+
+fun invokeOverrideEqual() {
+    println(Point(10, 20) == Point(10, 20))
+    println(Point(10, 20) != Point(10, 20))
+    println(Point(10, 20) == null)
+}
+
+fun invokeOverrideCompareTo() {
+    val p1 = Person("Alice", "Smith")
+    val p2 = Person("Bob", "Johnson")
+    println(p1 < p2)
+}
+
+fun invokeOverrideGetter() {
+    val p = Point(10, 20)
+    println(p[1])
+}
+
+fun invokeOverrideSetter() {
+    val p = MutablePoint(10,20)
+    p[1] = 42
+    println(p)
+}
+
+fun invokeOverrideContains() {
+    val rect = Rectangle(Point(10, 20), Point(50, 50))
+    println(Point(20, 30) in rect)
+    println(Point(5, 5) in rect)
+}
+
+fun invokeOverrideRangeTo() {
+    val now = LocalDate.now()
+    val vacation = now..now.plusDays(10)
+    println(now.plusWeeks(1) in vacation)
+}
+
+fun invokeOverrideClosedRangeIterator() {
+    val newYear = LocalDate.ofYearDay(2017, 1)
+    val daysOff = newYear.minusDays(1)..newYear
+    daysOff.iterator().forEach { println(it) }
+}
+
+fun invokeOverrideNameComponents() {
+//    val (name, ext) = splitFilename("example.kt")
+//    println(name)
+//    println(ext)
+
+    val (name, extension) = splitFileName("example.kt")
+    println(name)
+    println(extension)
 }
--- src/kia/sec7/foo.kt
+++ src/kia/sec7/foo.kt
@@ -1,12 +1,101 @@
 package kia.sec7
 
+import java.time.LocalDate
+
 data class Point(val x: Int, val y: Int) {
     operator fun plus(other: Point): Point {
         return Point(x + other.x, y + other.y)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other === this) return true
+        if (other !is Point) return false
+        return other.x == x && other.y == y
     }
 }
 
 data class PointSample(val x: Int, val y: Int)
 operator fun PointSample.plus(other: PointSample): PointSample {
     return PointSample(x + other.x, y + other.y)
+}
+
+operator fun Point.times(scale: Double): Point {
+    return Point((x * scale).toInt(), (y * scale).toInt())
+}
+
+operator fun Char.times(count: Int): String = toString().repeat(count)
+
+operator fun <T> MutableCollection<T>.plusAssign(element: T) {
+    // this.add(element)
+    add(element)
+}
+
+operator fun Point.unaryMinus(): Point {
+    return Point(-x, -y)
+}
+
+operator fun Point.inc(): Point {
+    return Point(x + 1, y + 1)
+}
+
+class Person(private val firstName: String, private val lastName: String): Comparable<Person> {
+    override fun compareTo(other: Person): Int {
+        return compareValuesBy(this, other, Person::lastName, Person::firstName)
+    }
+}
+
+operator fun Point.get(index: Int): Int {
+    return when(index) {
+        0 -> x
+        1 -> y
+        else ->
+            throw IndexOutOfBoundsException("Invalid coordinate $index")
+    }
+}
+
+data class MutablePoint(var x: Int, var y: Int) {
+    operator fun set(index: Int, value: Int) {
+        when(index) {
+            0 -> x = value
+            1 -> y = value
+            else -> throw IndexOutOfBoundsException("Invalid coordinate $index")
+        }
+    }
+}
+
+data class Rectangle(val upperLeft: Point, val lowerRight: Point) {
+    operator fun contains(p: Point): Boolean {
+        return p.x in upperLeft.x until lowerRight.x &&
+                p.y in upperLeft.y until lowerRight.y
+    }
+}
+
+operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> =
+        object : Iterator<LocalDate> {
+            var current = start
+
+            override fun hasNext(): Boolean = current <= endInclusive
+            override fun next() = current.apply { current = plusDays(1) }
+        }
+
+class FooPoint(private val x: Int, private val y: Int) {
+    operator fun component1() = x
+    operator fun component2() = y
+}
+
+data class NameComponents(val name: String, val extension: String)
+fun splitFilename(fullName: String): NameComponents {
+    val result = fullName.split('.', limit = 2)
+    return NameComponents(result[0], result[1])
+}
+
+fun splitFileName(fullName: String): NameComponents {
+    val (name, extension) = fullName.split('.', limit = 2)
+    return NameComponents(name, extension)
+}
+
+fun printEntries(map: Map<String, String>) {
+    for ((key, value) in map) {
+        println("$key -> $value")
+    }
 }
